bf8354a48955a79b290052fb580121553df7cab7
boydwNSU <47089310+boydwNSU@users.noreply.github.com>
2019-02-21 15:30:28 -0600

    TypingGame
    Before I start screwing with it

---
+++ Brackey's Typing Game/Assets/Scripts/LoadWords.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LoadWords : MonoBehaviour {
+
+	
+	void readString()
+    {
+        string path = "Assets/Resources/test.cpp";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        Debug.Log(reader.ReadToEnd());
+        reader.Close();
+        Debug.Log("hello");
+    }
+	
+}
