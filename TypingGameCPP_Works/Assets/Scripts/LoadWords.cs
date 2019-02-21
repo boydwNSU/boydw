bec0c87f8f74960ba322e3261bf1b06f69c0336d
boydwNSU <47089310+boydwNSU@users.noreply.github.com>
2019-02-21 16:40:35 -0600

    TypingGameCPP_Works
    Got the easy level of the cpp file working

---
+++ TypingGameCPP_Works/Assets/Scripts/LoadWords.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LoadWords : MonoBehaviour
+{
+    public TextAsset testing;
+
+    void Start()
+    {
+        TextAsset testing = Resources.Load<TextAsset>("test");
+
+        string[] data = testing.text.Split(new char[] { '\n' });
+
+        for (int i = 0; i < data.Length; i++)
+        {
+
+            Debug.Log(data[i]);
+
+        }
+    }
+
+}
