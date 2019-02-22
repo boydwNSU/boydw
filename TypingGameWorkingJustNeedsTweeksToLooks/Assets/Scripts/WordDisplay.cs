9a24106465a4d5ecae0658a9289665d3b59b662f
boydwNSU <47089310+boydwNSU@users.noreply.github.com>
2019-02-21 19:12:59 -0600

    TypingGameWorking
    It works, I jsut gotta make it look good

---
+++ TypingGameWorkingJustNeedsTweeksToLooks/Assets/Scripts/WordDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class WordDisplay : MonoBehaviour
+{
+    public Text text;
+    public float fallspeed = .3f;
+    
+
+    public void SetWord (string word)
+    {
+        text.text = word;
+    }
+
+    public void RemoveLetter()
+    {
+        text.text = text.text.Remove(0, 1);
+        text.color = Color.red;
+    }
+	
+    public void RemoveWord()
+    {
+        Destroy(gameObject);
+    }
+    
+    private void Update()
+    {
+        transform.Translate(0f, -fallspeed * Time.deltaTime, 0f);
+    }
+
+}
