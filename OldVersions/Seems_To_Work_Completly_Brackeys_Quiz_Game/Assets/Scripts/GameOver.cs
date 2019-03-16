a8f7a80e717bb5e6fe4ba48785b762cbd03e56a1
boydwNSU <47089310+boydwNSU@users.noreply.github.com>
2019-03-15 19:44:04 -0500

    fixed?

---
+++ OldVersions/Seems_To_Work_Completly_Brackeys_Quiz_Game/Assets/Scripts/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    public Text endScreenScore;
+    public int playerScore;
+
+    void Start()
+    {
+        playerScore = PlayerPrefs.GetInt("Player Score");
+        Debug.Log(playerScore);
+        endScreenScore.text = "Score: " + playerScore.ToString();
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MenuScreen");
+        
+    }
+
+
+}
---
+++ OldVersions/TypingGameWorkingJustNeedsTweeksToLooks/Assets/Resources/Medium.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	string userName; 
+	cout << "Enter name: ";
+	cin >> userName;
+	cout << "Your name is: " << userName << endl;
+}
