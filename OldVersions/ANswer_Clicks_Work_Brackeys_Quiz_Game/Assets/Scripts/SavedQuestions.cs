3aaec7cc9b2fd71c490bc8b94bcf4ac8fb871455
boydwNSU <47089310+boydwNSU@users.noreply.github.com>
2019-03-15 19:35:33 -0500

    ai, player, and sounds work

--- OldVersions/ANswer_Clicks_Work_Brackeys_Quiz_Game/Assets/Scripts/SavedQuestions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SavedQuestions
-{
-    public string yo, tú, él_ella_usted, nosotros_nosotras, ustedes, ellos_ellas, I, you_informal, he_she_you_formal, we_masculine_we_feminine, youAll, they_masculine_they_feminine;
-}
